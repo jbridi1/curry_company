a9f391224e9a45bd20add3c8e4b3fee2d5f2a14c
Jean <133663867+jbridi1@users.noreply.github.com>
2023-07-09 17:29:28 -0300

    Add files via upload

--- pages/1_Visao_Empresa.py
+++ pages/1_Visao_Empresa.py
@@ -185,7 +185,7 @@ st.sidebar.markdown ("""---""")
 st.sidebar.markdown ('## Selecione uma data limite')
 
 date_slider  = st.sidebar.slider(
-    'Até qual valor?', 
+    'Defina as Datas', 
     value=pd.datetime( 2022, 4, 13 ),
     min_value=pd.datetime( 2022, 2, 11 ),
     max_value=pd.datetime( 2022, 4, 6 ),
